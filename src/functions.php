0b815c267de9f4edb64a84a9eccc95a4754f8908
khairihz <khairi.hezzi@gmail.com>
2019-12-06 23:50:17 +0100

    add helper functions

---
+++ src/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Carbon\Carbon;
+use Yasumi\Provider\AbstractProvider;
+use Yasumi\Yasumi;
+
+function carbon(?DateTimeInterface $date): Carbon
+{
+    if (null === $date) {
+        return Carbon::now();
+    }
+
+    return Carbon::createFromFormat(DateTimeInterface::ATOM, $date->format(DateTimeInterface::ATOM));
+}
+
+function holidays(?DateTimeInterface $date, string $region = 'France'): AbstractProvider
+{
+    return Yasumi::create($region, (int)carbon($date)->format('Y'));
+}
