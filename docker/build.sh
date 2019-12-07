40090102d8a9c63ceaf9a073ac33e694673b96ae
khairihz <khairi.hezzi@gmail.com>
2019-12-07 23:07:36 +0100

    add docker scripts

---
+++ docker/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Install Yarn
+curl -o- -L https://yarnpkg.com/install.sh | bash
+ln -s ~/.yarn/bin/yarn /usr/local/bin/yarn
+
+echo "php_admin_flag[log_errors]=On" >>
+
+# Export bin directories
+export PATH="$PATH:/usr/www/louvre/vendor/bin:/var/www/louvre/bin"
